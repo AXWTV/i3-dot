b89d00436e5fca1ec45e532cb659113d41bf4c60
AXWTV <azmainsharer@gmail.com>
2023-06-03 21:07:43 +0600

    dotupdate

--- Dotfile.sh
+++ Dotfile.sh
@@ -13,7 +13,7 @@ printf "\e[1;34m
        ░                                                                                                                             
 ┌──────────────────────────────────────┐
 │ [1]  Backup                          │
-│ [2]  Restore                         │
+│ [2]  Restore error                   │
 │ [3]  Installation deb system .file   │ 
 │ [4]  Installation arch syatem .file  │
 │ [5]  Installation redhat system .file│
@@ -34,16 +34,19 @@ case "$OPTION" in
 	rm -r .zshrc
 	rm -r .config/neofetch
   rm -r .confin/nvim
-  rm -r .wallpaper
-	
-	cp -r ~/.config/alacritty/ .config/alacritty
-	cp -r ~/.config/nvim/ .config/nvim
-  cp -r ~/.config/polybar/ .config/polybar
-	cp -r ~/.config/i3/ .config/i3
-	cp -r ~/.config/picom/ .config/picom
+  rm -r .config/rofi
+	rm -r .config/nitrogen
+
+	cp -r ~/.config/alacritty .config/alacritty
+	cp -r ~/.config/nvim .config/nvim
+  cp -r ~/.config/polybar .config/polybar
+	cp -r ~/.config/i3 .config/i3
+	cp -r ~/.config/picom .config/picom
 	cp -r ~/.config/neofetch .config/neofetch
 	cp -r ~/.zshrc .zshrc
-	cp -r ~/.wallpaper .wallpaper
+  cp -r ~/color color
+  cp -r ~/.config/rofi .config/rofi
+  cp -r ~/.config/nitrogen .config/nitrogen
 
 	git add .
 	git commit -m "backup"
@@ -57,26 +60,26 @@ case "$OPTION" in
 	rm -r ~/.config/neofetch
   rm -r ~/.config/nvim
 	rm -r ~/.zshrc
-	rm -r ~/.wallpaper
 
-	mkdir -p ~/.config/i3
+  mkdir -p ~/.config/i3
 	mkdir -p ~/.config/polybar
 	mkdir -p ~/.config/picom
 	mkdir -p ~/.config/alacritty
-	mkdir -p ~/.config/neofetch
   mkdir -p ~/.config/nvim
+	mkdir -p ~/.config/neofetch
+  mkdir -p ~/.config/rofi
+  mkdir -p ~/.config/nitrogen
+  mkdir -p ~/.config/wallpaper
 
-
-  cp .config/i3/config ~/.config/i3/config
-	cp .config/nvim ~/.config/nvim
-  cp .config/polybar/config.ini ~/.config/polybar/config.ini
-	cp .config/polybar/launch.sh ~/.config/polybar/launch.sh
-	chmod +x ~/.config/polybar/launch.sh
-	cp .config/picom/picom.conf ~/.config/picom/picom.conf
-	cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-	cp -r .zshrc ~/.zshrc
-	cp .config/neofetch/config.conf ~/.config/neofetch/config.conf
-	cp -r .wallpaper ~/.wallpaper
+	cp -r .config/i3 ~/.config/i3
+  cp -r .config/nvim ~/.config/nvim
+	cp -r .config/polybar ~/.config/polybar
+	cp -r .config/picom ~/.config/picom
+	cp -r .config/alacritty ~/.config/alacritty
+  cp -r color ~/color
+  cp -r .config/rofi ~/.config/rofi
+	cp -r .config/neofetch ~/.config/neofetch
+  cp -r .config/nitrogen ~/.config/nitrogen
 	;;
 3)
 	echo "starting installation..."
@@ -84,7 +87,7 @@ case "$OPTION" in
 	sudo apt update
 
 	echo "Installing Dependencies {Deb-Only}"
-	sudo apt install -y i3-gaps polybar alacritty rofi feh snapd
+	sudo apt install -y i3-gaps polybar alacritty rofi dunst feh snapd nitrogen vim neofetch neovim
 	sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
 	sudo apt install -y cmake meson pkg-config asciidoc curl neofetch nvim vim
 
@@ -97,30 +100,33 @@ case "$OPTION" in
 	sudo ninja -C build install
 	cd ..
 
-	echo "Configering Config Files"
+	echo "Configering Confg Files"
 	mkdir -p ~/.config/i3
 	mkdir -p ~/.config/polybar
 	mkdir -p ~/.config/picom
 	mkdir -p ~/.config/alacritty
-	mkdir -p ~/.config/neofetch
   mkdir -p ~/.config/nvim
+	mkdir -p ~/.config/neofetch
+  mkdir -p ~/.config/rofi
+  mkdir -p ~/.config/nitrogen
+  mkdir -p ~/.config/wallpaper
 
 	cd ~/Dotfiles
 
-	cp .config/i3/config ~/.config/i3/config
-  cp .config/nvim ~/.config/nvim/
-	cp .config/polybar/config.ini ~/.config/polybar/config.ini
-	cp .config/polybar/launch.sh ~/.config/polybar/launch.sh
-	chmod +x ~/.config/polybar/launch.sh
-	cp .config/picom/picom.conf ~/.config/picom/picom.conf
-	cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-	cp -r .zshrc ~/.zshrc
-	cp .config/neofetch/config.conf ~/.config/neofetch/config.conf
+	cp -r .config/i3 ~/.config/i3
+  cp -r .config/nvim ~/.config/nvim
+	cp -r .config/polybar ~/.config/polybar
+	cp -r .config/picom ~/.config/picom
+	cp -r .config/alacritty ~/.config/alacritty
+  cp -r color ~/color
+  cp -r .config/rofi ~/.config/rofi
+	cp -r .config/neofetch ~/.config/neofetch
+  cp -r .config/nitrogen ~/.config/nitrogen
 
 	echo "Cofigering Files Done!"
 
 	echo "wallpaper set up"
-	cp -r .wallpaper ~/.wallpaper
+	git clone https://github.com/AXWTV/AXWTV_AniWall.git ~/.config/wallpaper
 
 	echo "Font set up"
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
@@ -141,33 +147,35 @@ case "$OPTION" in
 	cd
 
 	echo "Installing Dependencies {Arch-Only}"
-	sudo pacman -S i3-gaps polybar alacritty neofetch rofi feh vim nvim && yay -S picom-git
+	sudo pacman -S i3-gaps polybar alacritty nitrogen dunst neofetch rofi feh vim neovim && yay -S picom-git
 
-	echo "Configering Config Files"
+  echo "Configering Confg Files"
 	mkdir -p ~/.config/i3
 	mkdir -p ~/.config/polybar
 	mkdir -p ~/.config/picom
 	mkdir -p ~/.config/alacritty
-	mkdir -p ~/.config/neofetch/
   mkdir -p ~/.config/nvim
-
+	mkdir -p ~/.config/neofetch
+  mkdir -p ~/.config/rofi
+  mkdir -p ~/.config/nitrogen
+  mkdir -p ~/.config/wallpaper
 
 	cd ~/Dotfiles
 
-	cp .config/i3/config ~/.config/i3/config
-	cp .config/nvim ~/.config/nvim/
-  cp .config/polybar/config.ini ~/.config/polybar/config.ini
-	cp .config/polybar/launch.sh ~/.config/polybar/launch.sh
-	chmod +x ~/.config/polybar/launch.sh
-	cp .config/picom/picom.conf ~/.config/picom/picom.conf
-	cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-	cp -r .zshrc ~/.zshrc
-	cp .config/neofetch/config.conf ~/.config/neofetch/config.conf
+	cp -r .config/i3 ~/.config/i3
+  cp -r .config/nvim ~/.config/nvim
+	cp -r .config/polybar ~/.config/polybar
+	cp -r .config/picom ~/.config/picom
+	cp -r .config/alacritty ~/.config/alacritty
+  cp -r color ~/color
+  cp -r .config/rofi ~/.config/rofi
+	cp -r .config/neofetch ~/.config/neofetch
+  cp -r .config/nitrogen ~/.config/nitrogen
 
 	echo "Cofigering Files Done!"
 
 	echo "wallpaper set up"
-	cp -r .wallpaper ~/.wallpaper
+	git clone https://github.com/AXWTV/AXWTV_AniWall.git ~/.config/wallpaper
 
 	echo "Font set up"
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
@@ -188,7 +196,8 @@ case "$OPTION" in
 	cd
 
 	echo "Installing Dependencies {Red_Hat-Only}"
-	sudo dnf install -y i3-gaps polybar alacritty neofetch cpufetch rofi feh
+  sudo dnf copr enable atim/bottom -y
+  sudo dnf install -y i3-gaps polybar bottom dunst alacritty neofetch rofi feh vim nitrogen neovim
 	sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel asciidoc
 
 	echo "picom building"
@@ -207,25 +216,28 @@ case "$OPTION" in
 	mkdir -p ~/.config/alacritty
   mkdir -p ~/.config/nvim
 	mkdir -p ~/.config/neofetch
+  mkdir -p ~/.config/rofi
+  mkdir -p ~/.config/nitrogen
+  mkdir -p ~/.config/wallpaper
 
 	cd ~/Dotfiles
 
-	cp .config/i3/config ~/.config/i3/config
-  cp .config/nvim ~/.config/nvim/
-	cp .config/polybar/config.ini ~/.config/polybar/config.ini
-	cp .config/polybar/launch.sh ~/.config/polybar/launch.sh
-	chmod +x ~/.config/polybar/launch.sh
-	cp .config/picom/picom.conf ~/.config/picom/picom.conf
-	cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-	cp -r .zshrc ~/.zshrc
-	cp .config/neofetch/config.conf ~/.config/neofetch/config.conf
+	cp -r .config/i3 ~/.config/i3
+  cp -r .config/nvim ~/.config/nvim
+	cp -r .config/polybar ~/.config/polybar
+	cp -r .config/picom ~/.config/picom
+	cp -r .config/alacritty ~/.config/alacritty
+  cp -r color ~/color
+  cp -r .config/rofi ~/.config/rofi
+	cp -r .config/neofetch ~/.config/neofetch
+  cp -r .config/nitrogen ~/.config/nitrogen
 
 	echo "Cofigering Files Done!"
 
 	echo "wallpaper set up"
-	cp -r .wallpaper ~/.wallpaper
-
-	echo "Font set up"
+	git clone https://github.com/AXWTV/AXWTV_AniWall.git ~/.config/wallpaper
+	
+  echo "Font set up"
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
 	wget https://use.fontawesome.com/releases/v6.2.1/fontawesome-free-6.2.1-desktop.zip
 	mkdir ~/.fonts
