613272468cf725fe69161a44634aaa5dd6883d01
AXWTV <azmainsharer@gmail.com>
2023-07-10 11:27:50 +0600

    update 1.8.0

--- DotFile_X.sh
+++ DotFile_X.sh
@@ -18,6 +18,7 @@ printf "\e[1;34m
 │ [4]  Build Picom                                   │
 │ [5]  .zshrc cp error                               │
 │ [6]  Fonts                                         │
+│ [7]  PowerLevel10K                                 │
 │ [0]  Exit                                          │
 └────────────────────────────────────────────────────┘
 \e[0m"
@@ -27,52 +28,47 @@ case "$OPTION" in
 
 1)
   cd
-  echo "Configering Confg Files"
-  mkdir -p ~/.config/i3
-  mkdir -p ~/.config/polybar
-  mkdir -p ~/.config/picom
-  mkdir -p ~/.config/alacritty
-  mkdir -p ~/.config/nvim
-  mkdir -p ~/.config/neofetch
-  mkdir -p ~/.config/rofi
-  mkdir -p ~/.config/nitrogen
-  mkdir -p ~/.config/dunst
+  echo "##################################"
+  echo "#    Configering Confg Files     #"
+  echo "##################################"
+
+  mkdir -p ~/.config/
 
   cd ~/Dotfiles
-  
-  cp -r .config/i3 ~/.config/
-  cp -r .config/nvim ~/.config/
-  cp -r .config/polybar ~/.config/
-  cp -r .config/picom ~/.config/
-  cp -r .config/alacritty ~/.config/
-  cp -r .zshrc ~/.zshrc
-  cp -r color ~/color
-  cp -r .config/rofi ~/.config/
-  cp -r .config/neofetch ~/.config/
-  cp -r .config/dunst ~/.config/
-  
-  echo "Cofigering Files Done!"
+ 
+  cp -r .config/* ~/.config/
+
+  echo "##################################"
+  echo "#     Cofigering Files Done!     #"
+  echo "##################################"
   ;;
 
 2)
   source .installation_system_software.sh
-  sleep 1
   cd ~/Dotfiles
-;;
+  echo "###################"
+  echo "#    Complete     #"
+  echo "###################"
+  ;;
   
-
 3)
-  echo "Oh-My-Zsh"
+  echo "###############"
+  echo "#  Oh-My-Zsh  #"
+  echo "###############"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   rm -r ~/.zshrc
   cp -r .zshrc ~/.zshrc
-  echo "installation Complite! run [7]"
+
+  echo "################################"
+  echo "#    installation Complete!    #"
+  echo "################################"
   ;;
 
 4)
   cd
- 
-  echo "picom building"
+  echo "##################"
+  echo "# picom building #"
+  echo "##################"
   git clone https://github.com/yshui/picom.git
   cd picom
   git submodule update --init --recursive
@@ -80,27 +76,54 @@ case "$OPTION" in
   ninja -C build
   sudo ninja -C build install
   cd ~/Dotfiles
+  echo "###################"
+  echo "#    Complete     #"
+  echo "###################"
   ;;
 
 5)
   rm -r ~/.zshrc
+  rm -r ~/.p10k.zsh
   cp -r .zshrc ~/.zshrc
+  cp -r .p10k.zsh ~/.p10k.zsh
+  echo "###################"
+  echo "#    Complete     #"
+  echo "###################"
   ;;
 
 6)
-  echo "fonts"
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
-  wget https://use.fontawesome.com/releases/v6.2.1/fontawesome-free-6.2.1-desktop.zip
-
-  mkdir ~/.fonts
-  cp JetBrainsMono.zip ~/.fonts/
-  cp fontawesome-free-6.2.1-desktop.zip ~/.fonts/
+  echo "#######################"
+  echo "#        Fonts        #"
+  echo "#######################"
+  mkdir -p ~/.fonts/
+  mkdir -p ~/.local/share/fonts
   cd ~/.fonts/
-  unzip JetBrainsMono.zip 
-  unzip fontawesome-free-6.2.1-desktop.zip 
-  cd ~/Dotfiles
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Iosevka.zip
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/NerdFontsSymbolsOnly.zip
+  wget https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-desktop.zip
+  unzip fontawesome-free-6.4.0-desktop.zip -d ~/.local/share/fonts
+  unzip Hack.zip -d ~/.local/share/fonts
+  unzip Iosevka.zip -d ~/.local/share/fonts
+  unzip JetBrainsMono.zip -d ~/.local/share/fonts
+  unzip NerdFontsSymbolsOnly.zip -d ~/.local/share/fonts
 
-  echo "Get Awesome Font and JetBrainMono Nerd Font Done!"
+  cd ~/Dotfiles
+  echo "###################################################" 
+  echo "#Get Awesome Font and JetBrainMono Nerd Font Done!#"
+  echo "###################################################"
+  ;;
+
+7)
+  echo "###################"
+  echo "#  PowerLevel10K  #"
+  echo "###################"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  cp -r .p10k.zsh ~/.p10k.zsh
+  echo "###################"
+  echo "#    Complete     #"
+  echo "###################"
   ;;
 
 0)
