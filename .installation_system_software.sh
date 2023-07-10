693b92e08aa485e47c57b04247109d0735c0722b
AXWTV <azmainsharer@gmail.com>
2023-07-10 11:48:14 +0600

    update-.installation_system_software.sh

--- .installation_system_software.sh
+++ .installation_system_software.sh
@@ -28,9 +28,10 @@ if [ "$DISTRO" == "Ubuntu" ]; then
     sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
     sudo apt install -y cmake meson pkg-config asciidoc curl neofetch
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
 
@@ -47,9 +48,10 @@ elif [ "$DISTRO" == "Fedora Linux" ]; then
     sudo dnf install -y i3-gaps polybar btop dunst alacritty neofetch neovim vim rofi feh nitrogen zsh
     sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-devel xcb-util-renderutil-devel xorg-x11-proto-devel asciidoc curl cmake
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
     echo "Done!"
@@ -67,10 +69,12 @@ elif [ "$DISTRO" == "Debian GNU/Linux" ]; then
     sudo apt install -y i3-gaps polybar alacritty rofi vim dunst neovim feh snapd zsh btop
     sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
     sudo apt install -y cmake meson pkg-config asciidoc curl neofetch
+
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
 
@@ -85,10 +89,12 @@ elif [ "$DISTRO" == "Arch Linux" ]; then
     #installing softwares
     echo "Installing Dependencies {Arch-Only}"
     sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl vim nvim && yay -S picom-git
+
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
     echo "Done!"
@@ -102,10 +108,12 @@ elif [ "$DISTRO" == "ArcoLinux" ]; then
     #installing softwares
     echo "Installing Dependencies {Arch-Only}"
     sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl vim nvim && yay -S picom-git
+
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
     echo "Done!"
@@ -119,10 +127,12 @@ elif [ "$DISTRO" == "EndeavourOS" ]; then
     #installing softwares
     echo "Installing Dependencies {Arch-Only}"
     sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl vim nvim && yay -S picom-git
+
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
     echo "Done!"
@@ -137,10 +147,12 @@ elif [ "$DISTRO" == "CentOS Linux" ]; then
     echo "Installing Dependencies {RHEL-Only}"
     sudo dnf install -y i3-gaps polybar btop dunst alacritty neofetch neovim vim rofi feh nitrogen zsh
     sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel asciidoc curl cmake
+
     #cloning rofi theme
-    git clone https://github.com/catppuccin/rofi.git
-    cd rofi/basic
-    ./install.sh
+    git clone https://github.com/adi1090x/rofi.git
+    cd rofi
+    chmod +x setup.sh
+    ./setup.sh
     
     cd ~/Dotfiles
     echo "Done!"
