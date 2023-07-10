bdc892052a763bb13037a231088b0837c4240f12
AXWTV <azmainsharer@gmail.com>
2023-07-10 12:42:14 +0600

    update - alacritty

--- .installation_system_software.sh
+++ .installation_system_software.sh
@@ -24,7 +24,7 @@ if [ "$DISTRO" == "Ubuntu" ]; then
     #installing and updating softwares
     sudo apt update
     echo "Installing Dependencies {Debian-Only}"
-    sudo apt install -y i3-gaps polybar alacritty rofi vim dunst neovim feh snapd zsh btop
+    sudo apt install -y i3-gaps polybar alacritty rofi vim dunst neovim feh snapd zsh btop lsd
     sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
     sudo apt install -y cmake meson pkg-config asciidoc curl neofetch
     #cloning rofi theme
@@ -45,7 +45,7 @@ elif [ "$DISTRO" == "Fedora Linux" ]; then
     cd
     #installing software
     echo "Installing Dependencies {RHEL-Only}"
-    sudo dnf install -y i3-gaps polybar btop dunst alacritty neofetch neovim vim rofi feh nitrogen zsh
+    sudo dnf install -y i3-gaps polybar btop dunst alacritty neofetch neovim vim rofi feh nitrogen zsh lsd
     sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-devel xcb-util-renderutil-devel xorg-x11-proto-devel asciidoc curl cmake
     #cloning rofi theme
     git clone https://github.com/adi1090x/rofi.git
@@ -66,7 +66,7 @@ elif [ "$DISTRO" == "Debian GNU/Linux" ]; then
     #installing and updating softwares
     sudo apt update
     echo "Installing Dependencies {Debian-Only}"
-    sudo apt install -y i3-gaps polybar alacritty rofi vim dunst neovim feh snapd zsh btop
+    sudo apt install -y i3-gaps polybar alacritty rofi vim dunst neovim feh snapd zsh btop lsd
     sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
     sudo apt install -y cmake meson pkg-config asciidoc curl neofetch
 
@@ -88,7 +88,7 @@ elif [ "$DISTRO" == "Arch Linux" ]; then
     cd
     #installing softwares
     echo "Installing Dependencies {Arch-Only}"
-    sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl vim nvim && yay -S picom-git
+    sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh lsd feh curl vim nvim && yay -S picom-git
 
     #cloning rofi theme
     git clone https://github.com/adi1090x/rofi.git
@@ -107,7 +107,7 @@ elif [ "$DISTRO" == "ArcoLinux" ]; then
     cd
     #installing softwares
     echo "Installing Dependencies {Arch-Only}"
-    sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl vim nvim && yay -S picom-git
+    sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl lsd vim nvim && yay -S picom-git
 
     #cloning rofi theme
     git clone https://github.com/adi1090x/rofi.git
@@ -126,7 +126,7 @@ elif [ "$DISTRO" == "EndeavourOS" ]; then
     cd
     #installing softwares
     echo "Installing Dependencies {Arch-Only}"
-    sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop dunst zsh feh curl vim nvim && yay -S picom-git
+    sudo pacman -S i3-gaps polybar alacritty neofetch rofi btop lsd dunst zsh feh curl vim nvim && yay -S picom-git
 
     #cloning rofi theme
     git clone https://github.com/adi1090x/rofi.git
@@ -145,7 +145,7 @@ elif [ "$DISTRO" == "CentOS Linux" ]; then
     cd
     #installing software
     echo "Installing Dependencies {RHEL-Only}"
-    sudo dnf install -y i3-gaps polybar btop dunst alacritty neofetch neovim vim rofi feh nitrogen zsh
+    sudo dnf install -y i3-gaps polybar btop dunst alacritty neofetch neovim vim rofi feh nitrogen zsh lsd
     sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel asciidoc curl cmake
 
     #cloning rofi theme
